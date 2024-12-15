21fa53bc4d5a65a473658816137a8f35a9faeb9c
Biswajit Jana <biswajitworkmail@gmail.com>
2024-12-15 11:24:23 +0530

    default intigration

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
+const globals =require ('globals');
+const pluginJs =require ( '@eslint/js');
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+module.exports= [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended
